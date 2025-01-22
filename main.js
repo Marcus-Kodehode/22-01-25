456d3f61e1b9707003325a549941f5d601b98daf
Marcus-Kodehode <marcus29sfj@gmail.com>
2025-01-22 12:34:00 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// const container = document.getElementById("container");
+
+// console.log(container);
+
+// container.style.backgroundColor = "orange";
+
+const img = document.getElementById("container");
+
+function makeSmall() {
+  img.style.height = "0px";
+  img.style.width = "0px";
+}
+
+function makeNormal() {
+  img.style.height = "200px";
+  img.style.width = "200px";
+}
+
+function makeBig() {
+  img.style.height = "400px";
+  img.style.width = "400px";
+}
+
+console.log(img);
+
+function start() {
+  return true;
+}
+start();
+
+const greeting = "hello world";
+
+console.log(greeting);
+
+// if (3 % 2 === 0) {
+//   document.body.style.backgroundColor = "orange";
+// } else {
+//   document.body.style.backgroundColor = "blue";
+// }
